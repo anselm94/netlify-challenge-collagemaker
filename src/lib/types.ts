3dbea16717bd3b4367e81514cae54dfc7785efdf
Merbin J Anselm <merbin.j.anselm@sap.com>
2024-05-10 17:05:11 +0530

    delete sveltekit based implementation

--- src/lib/types.ts
+++
@@ -1,6 +0,0 @@
-export type GalleryMetadata = {
-	id: string;
-	lastModified: number;
-	layout: 'grid-2' | 'grid-3' | 'grid-4' | 'grid-5' | 'grid-6' | 'grid-7' | 'grid-8';
-	images: Array<string | null>;
-};
